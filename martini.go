709eeb5419cd8cd173b06cf0045515effad1cf1e
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-10-29 21:31:21 -0700

    Removed context. Put everything in martini.go

--- martini.go
+++ martini.go
@@ -3,6 +3,7 @@ package martini
 import (
 	"github.com/codegangsta/inject"
 	"net/http"
+	"reflect"
 )
 
 type Martini struct {
@@ -23,4 +24,50 @@ func (m *Martini) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	ctx.MapTo(res, (*http.ResponseWriter)(nil))
 	ctx.Map(req)
 	ctx.run()
+}
+
+type Context interface {
+	inject.Injector
+	Next()
+}
+
+type context struct {
+	injector inject.Injector
+	handlers []interface{}
+	index    int
+}
+
+func (c *context) Invoke(f interface{}) error {
+	return c.injector.Invoke(f)
+}
+
+func (c *context) Map(val interface{}) {
+	c.injector.Map(val)
+}
+
+func (c *context) MapTo(val interface{}, ifacePtr interface{}) {
+	c.injector.MapTo(val, ifacePtr)
+}
+
+func (c *context) Get(t reflect.Type) reflect.Value {
+	return c.injector.Get(t)
+}
+
+func (c *context) SetParent(p inject.Injector) {
+	c.injector.SetParent(p)
+}
+
+func (c *context) Next() {
+	c.index += 1
+	c.run()
+}
+
+func (c *context) run() {
+	for c.index < len(c.handlers) {
+		err := c.Invoke(c.handlers[c.index])
+		if err != nil {
+			panic(err)
+		}
+		c.index += 1
+	}
 }
