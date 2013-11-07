960fb0170d8ab01449cef7bff3018461b343217c
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-11-06 21:20:12 -0800

    Real working basic auth

--- auth/basic.go
+++ auth/basic.go
@@ -1,17 +1,62 @@
 package auth
 
 import (
+	"encoding/base64"
 	"net/http"
+	"strings"
 )
 
 func Basic(name string, password string) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
-		u := req.URL.User.Username()
-		p, _ := req.URL.User.Password()
+		auth, ok := req.Header["Authorization"]
+		if !ok {
+			unauthorized(res)
+		}
+
+		u, p := parseToken(auth[0])
 
 		if u != name || p != password {
-			res.Header().Set("WWW-Authenticate", "Basic realm=\"Authorization Required\"")
-			res.WriteHeader(401)
+			unauthorized(res)
 		}
 	}
+}
+
+func unauthorized(res http.ResponseWriter) {
+	res.Header().Set("WWW-Authenticate", "Basic realm=\"Authorization Required\"")
+	res.WriteHeader(http.StatusUnauthorized)
+}
+
+// ParseToken is a helper function that extracts the username and password
+// from an authorization token.  Callers should be able to pass in the header
+// "Authorization" from an HTTP request, and retrieve the credentials.
+func parseToken(token string) (username, password string) {
+	if token == "" {
+		return "", ""
+
+	}
+
+	// Check that the token supplied corresponds to the basic authorization
+	// protocol
+	ndx := strings.IndexRune(token, ' ')
+	if ndx < 1 || token[0:ndx] != "Basic" {
+		return "", ""
+
+	}
+
+	// Drop prefix, and decode the base64
+	buffer, err := base64.StdEncoding.DecodeString(token[ndx+1:])
+	if err != nil {
+		return "", ""
+
+	}
+	token = string(buffer)
+
+	ndx = strings.IndexRune(token, ':')
+	if ndx < 1 {
+		return "", ""
+
+	}
+
+	return token[0:ndx], token[ndx+1:]
+
 }
