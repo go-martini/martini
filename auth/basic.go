abc2f6607a7f8526b0dea8e1eccf428544f08e49
Brendon Murphy <xternal1+github@gmail.com>
2013-11-12 20:33:32 -0800

    Use a secure comparison in auth middleware
    Otherwise you might get timing attacks:
    
    http://verboselogging.com/2012/08/20/a-timing-attack-in-action

--- auth/basic.go
+++ auth/basic.go
@@ -11,7 +11,7 @@ func Basic(username string, password string) http.HandlerFunc {
 	var siteAuth = base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
 	return func(res http.ResponseWriter, req *http.Request) {
 		auth := req.Header.Get("Authorization")
-		if auth != "Basic "+siteAuth {
+		if !SecureCompare(auth, "Basic "+siteAuth) {
 			res.Header().Set("WWW-Authenticate", "Basic realm=\"Authorization Required\"")
 			http.Error(res, "Not Authorized", http.StatusUnauthorized)
 		}
