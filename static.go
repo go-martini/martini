9ed873e1f12459c5cea35a3ff975f0a8c5925054
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-11-14 15:06:07 -0800

    JMS #37: call filepath.Clean() on static path to stop dir traversal
    We are also now logging in the static handler when we serve up a file
    
    closes #37

--- static.go
+++ static.go
@@ -1,6 +1,7 @@
 package martini
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -8,10 +9,11 @@ import (
 
 // Static returns a middleware handler that serves static files in the given path.
 func Static(path string) Handler {
-	return func(res http.ResponseWriter, req *http.Request) {
-		file := filepath.Join(path, req.URL.Path)
+	return func(res http.ResponseWriter, req *http.Request, log *log.Logger) {
+		file := filepath.Join(path, filepath.Clean(req.URL.Path))
 		info, err := os.Stat(file)
 		if err == nil && !info.IsDir() {
+			log.Println("[Static] Serving " + file)
 			http.ServeFile(res, req, file)
 		}
 	}
