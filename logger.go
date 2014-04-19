9399467362071fec5b06c162e99f7d0457e7face
Julius Francisco <baldrailers@gmail.com>
2014-04-20 02:22:43 +0800

    Adding RemoteAddr for logger requests

--- logger.go
+++ logger.go
@@ -1,20 +1,20 @@
 package martini
 
 import (
-	"log"
-	"net/http"
-	"time"
+  "log"
+  "net/http"
+  "time"
 )
 
 // Logger returns a middleware handler that logs the request as it goes in and the response as it goes out.
 func Logger() Handler {
-	return func(res http.ResponseWriter, req *http.Request, c Context, log *log.Logger) {
-		start := time.Now()
-		log.Printf("Started %s %s", req.Method, req.URL.Path)
+  return func(res http.ResponseWriter, req *http.Request, c Context, log *log.Logger) {
+    start := time.Now()
+    log.Printf("Started %s %s for %s", req.Method, req.URL.Path, req.RemoteAddr)
 
-		rw := res.(ResponseWriter)
-		c.Next()
+    rw := res.(ResponseWriter)
+    c.Next()
 
-		log.Printf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start))
-	}
+    log.Printf("Completed %v %s in %v for %s\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start), req.RemoteAddr)
+  }
 }
